79ae2449bef635a83333593d8890879dfe0bd14a
Joel-Kachulu <101320267+Joel-Kachulu@users.noreply.github.com>
2025-05-20 21:13:24 +0000

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,13 +1,14 @@
 #!/bin/env python
 from app import create_app, socketio
-#import db too if using the in app context()
+import os
+import db too if using the in app context()
 
 app = create_app(debug=True, host='0.0.0.0')
 
-#with app.app_context():
-#    db.create_all()
+with app.app_context():
+    db.create_all()
 #unnecessary change
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=3000)
-    socketio.run(app, host='192.168.126.2', port=3000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
